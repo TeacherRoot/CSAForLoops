1d52162e4187a6c1203302ba03b8441742aa05c9
Ann Root <ann.root@bvsd.org>
2024-10-08 10:57:18 -0600

    Create ForLoops.java

---
+++ ForLoops.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ForLoops {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Assignment 1 here");
+
+        System.out.println("Assignment 2 here");
+        System.out.println("Please enter a value to count to");
+      
+        int countTo = scan.nextInt();
+
+
+        System.out.println("Assignment 3 here");
+
+
+
+        System.out.println("Assignment 4 here");
+
+
+
+    }
+
+}
